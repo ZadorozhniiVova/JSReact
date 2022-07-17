93f04235b9ed2517ebefd3351a2fc9e1829d97be
Vova <zadorozhniiv@gmail.com>
2022-07-17 21:57:59 +0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Last watched film?', ''),
-      b = prompt('Rate of it?', ''),
-      c = prompt('Last watched film?', ''),
-      d = prompt('Rate of it?', '');
+// const a = prompt('Last watched film?', ''),
+//       b = prompt('Rate of it?', ''),
+//       c = prompt('Last watched film?', ''),
+//       d = prompt('Rate of it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+
+// УСЛОВИЯ
+
+// if (4 == 9){
+//     console.log('ok!');
+// } else{
+//     console.log('error');
+// }
+
+
+
+
+
+const num = '50';
+
+// if( num < 49){
+//     console.log('Error');
+// } else if (num>100){
+//     console.log('A lot');
+// } else{
+//     console.log('Ok!')
+// }
+
+
+// (num === 50) ? console.log('Ok!') : console.log('Error'); 
+
+
+// switch(num){
+//     case '49':
+//         console.log('Not correct');
+//         break;
+//     case '100':
+//         console.log('Not correct');
+//         break;
+//     case '50':
+//         console.log('Correct');
+//         break;
+//     default:
+//         console.log('not this time');
+//         break    
+
+        
+// }
