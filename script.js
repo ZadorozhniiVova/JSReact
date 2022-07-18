7e9ae0bf714e977851fe61033bc37ef0d7b79562
Vova <zadorozhniiv@gmail.com>
2022-07-18 14:17:39 +0300

    update

--- script.js
+++ script.js
@@ -65,3 +65,54 @@
 
 
 // ЛОГИЧЕСКИЕ ОПЕРАТОРЫ
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('ok');
+// }
+
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'string');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все сыты');
+// } else {
+//     console.log('Мы уходим')
+// };
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны');
+// } else {
+//     console.log('Мы уходим')
+// };
+
+// console.log(hamburger || cola || fries);
+
+
+// let johnReport, AlexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || AlexReport || samReport || mariaReport);
+
+
+
+
+// Homework
+
+console.log(5 === 5 && 3 > 1 || 5);
